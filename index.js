8b6331482471395782573a1eeaf410c237a84acf
Antonio <antbertozzi@hotmail.com>
2020-08-26 08:11:57 -0300

    feat: express installed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Unifafibe');
+const express = require('express');
+const server = express();
+
+server.get('/boasvindas',function(request, response){
+    response.send('<h1>BEM VINDO A NOSSA API!</H1>');
+});
+
+server.listen(3333, function(){
+    console.log('O PAI TÁ ON!');
+});
+
